026c9b9889cbb14eb19a883b3cc8e36f8d7defc7
AmirMohammad-Rezaei <amirrezaei1133@gmail.com>
2022-09-03 16:01:12 +0430

    second commit

---
+++ lib/page/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class home extends StatefulWidget {
+  const home({Key? key}) : super(key: key);
+
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      body: Column(
+        children: const [],
+      ),
+    ));
+  }
+}
